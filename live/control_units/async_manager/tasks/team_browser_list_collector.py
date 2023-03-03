fd3f13007d1980a9bb6c7e7e9511fea15bf9a496
“Baalt” <enjoylifebalt@gmail.com>
2023-03-03 21:06:28 +0200

    Add MatchCollector class to team_browser_list_collector.py
    This commit adds the MatchCollector class to team_browser_list_collector.py.
    The MatchCollector class is responsible for collecting match data from live_dict and smart_dict and storing it in self.matches dictionary.
    It uses FootballTeamsComparison class to compare team names and filter out irrelevant matches.
    The collected matches can be accessed using the get_matches method.

--- live/control_units/async_manager/tasks/team_browser_list_collector.py
+++ live/control_units/async_manager/tasks/team_browser_list_collector.py
@@ -1,17 +1,23 @@
-import asyncio
-from typing import Tuple
-from selenium import webdriver
+from live.control_units.async_manager.tasks.command_matcher import FootballTeamsComparison
 
-from async_live.control_units.async_manager.tasks.command_matcher import FootballTeamsComparison
+
+from typing import Tuple
 
 
 class MatchCollector:
-    def __init__(self):
+    def __init__(self, matches: dict):
         self.filter = FootballTeamsComparison()
+        self.matches = matches
 
-    async def collect_matches(self, live_dict: dict, smart_dict: dict,
-                              browsers: list, url='https://www.fon.bet/') -> None:
+    def collect_matches(self, live_dict: dict, smart_dict: dict) -> None:
+        # Remove any keys from self.matches that are not in live_dict keys
+        for match_key in list(self.matches.keys()):
+            if match_key not in live_dict:
+                del self.matches[match_key]
+
         for live_key, live_value in live_dict.items():
+            if live_key in self.matches:
+                continue
             live_home_name, live_away_name = self._split_command_names(command_names=live_key)
             for smart_data in smart_dict['list']:
                 smart_home_name, smart_away_name = smart_data['home_command_name'], smart_data['away_command_name']
@@ -22,16 +28,18 @@ class MatchCollector:
                     smart_team_2=smart_away_name
                 )
                 if match:
-                    # создать обьект для управления браузером, со сценарием и вычисления, а менеджер уже в свою очередь будет
-                    # выполнять алгоритм поиска
-                    self._open_browser(browsers, url)
+                    match_data = {
+                        'url': live_value,
+                        'live_team_1': live_home_name,
+                        'live_team_2': live_away_name,
+                        'smart_data': smart_data
+                    }
+                    self.matches[live_key] = match_data
 
-        await asyncio.gather(*browsers)
-
-    def _open_browser(self, browsers: list, url: str) -> None:
-        driver = webdriver.Chrome()
-        driver.get(url)
 
     def _split_command_names(self, command_names: str, commands_separator=' — ') -> Tuple[str, str]:
         home_team, away_team = command_names.split(commands_separator)
         return home_team, away_team
+
+    def get_matches(self) -> dict:
+        return self.matches
