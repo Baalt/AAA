8489963cd0e25713df537d76e815b4e879de5c59
“Baalt” <enjoylifebalt@gmail.com>
2023-03-06 19:18:42 +0200

    Move SmartDataHandler to PickleHandler module for improved organization.
    This commit moves the SmartDataHandler class from live/control_units/manager/tasks/data_handler.py
    to toolz/pickle_manager.py and renames it to PickleHandler.
    The data_handler module has been removed since it is no longer needed.
    
    This change improves code organization and clarity by consolidating related functionality into a single module.

---
+++ toolz/pickle_manager.py
@@ -0,0 +1,12 @@
+import pickle
+
+
+class PickleHandler:
+    def read_data(self, path_to_file="live/control_units/async_manager/smart_data.pkl"):
+        with open(path_to_file, "rb") as f:
+            data = pickle.load(f)
+        return data
+
+    def write_data(self, data: dict, path_to_file="live/control_units/async_manager/smart_data.pkl"):
+        with open(path_to_file, "wb") as f:
+            pickle.dump(data, f)
