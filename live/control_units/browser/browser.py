f3b3fde4115acc18079f1a6c0e9aeb6ca1207308
“Baalt” <enjoylifebalt@gmail.com>
2023-03-06 18:28:53 +0200

    Refactored MultipleTabsChromeDriver to support asynchronous execution of scripts and added a new method to open all URLs in multiple tabs. Also added a browser_preparer argument to switch language before opening the main URL.

--- live/control_units/browser/browser.py
+++ live/control_units/browser/browser.py
@@ -1,65 +1,22 @@
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from async_live.control_units.browser.buttons.menu_buttons import Buttons
+from live.control_units.browser.buttons.menu_buttons import Buttons
 
 
 class HeadlessChromeDriver:
     def __init__(self, driver_path: str = '/usr/local/bin/chromedriver', headless: bool = True):
         options = Options()
         options.headless = headless
-        if headless:
-            options.add_argument('--disable-gpu')
         service = Service(executable_path=driver_path)
         self.driver = webdriver.Chrome(service=service, options=options)
         self.buttons = Buttons(browser=self.driver)
         self.html = None
 
     def open_page(self, url):
-        self.driver.get(url)
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-        wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-
-    def get_page_html(self):
-        return self.driver.page_source
-
-
-    def maximize_window(self):
-        self.driver.maximize_window()
-
-    def close(self):
-        self.driver.quit()
-
-
-
-import asyncio
-
-class AsyncHeadlessChromeDriver:
-    def __init__(self, driver_path: str = '/usr/local/bin/chromedriver', headless: bool = True):
-        options = Options()
-        options.headless = headless
-        if headless:
-            options.add_argument('--disable-gpu')
-        service = Service(executable_path=driver_path)
-        self.driver = None
-        self.service = service
-        self.options = options
-        self.buttons = None
-        self.html = None
-
-    async def open_page(self, url):
-        if not self.driver:
-            self.driver = await asyncio.to_thread(webdriver.Chrome, service=self.service, options=self.options)
-            self.buttons = Buttons(browser=self.driver)
-        await asyncio.to_thread(self.driver.execute_script, 'window.open()')
-        await asyncio.to_thread(self.driver.switch_to.window, self.driver.window_handles[-1])
-        await asyncio.to_thread(self.driver.get, url)
+        self.driver.execute_script("window.location.href = '{}';".format(url))
 
     def get_page_html(self):
         return self.driver.page_source
@@ -69,5 +26,25 @@ class AsyncHeadlessChromeDriver:
 
     def close(self):
         self.driver.quit()
-        self.driver = None
-        self.buttons = None
+
+
+class MultipleTabsChromeDriver:
+    def __init__(self, driver: HeadlessChromeDriver, smart_dict):
+        self.driver = driver
+        self.driver.maximize_window()
+        self.smart_dict = smart_dict
+
+    async def open_all_urls(self, browser_preparer):
+        tabs = list(self.smart_dict.keys())
+        main_tab = tabs[0]
+        self.driver.open_page(self.smart_dict[main_tab]['url'])
+        browser_preparer(driver=self.driver).switch_language()
+        tasks = []
+        for tab in tabs[1:]:
+            script = "window.open('{}', '_blank');".format(self.smart_dict[tab]['url'])
+            task = asyncio.create_task(self.execute_script_async(script))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+    async def execute_script_async(self, script):
+        await asyncio.to_thread(self.driver.driver.execute_script, script)
