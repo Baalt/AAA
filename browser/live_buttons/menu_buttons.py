d23595d19c14c9a87b3e36f18dee355366e5eb54
“Baalt” <enjoylifebalt@gmail.com>
2023-03-07 18:35:35 +0200

    Refactoring: Renamed method names in menu_buttons.py for greater clarity and consistency in the browser/live_buttons module.

--- browser/live_buttons/menu_buttons.py
+++ browser/live_buttons/menu_buttons.py
@@ -7,12 +7,12 @@ class LanguageButtons:
     def __init__(self):
         self.browser = None
 
-    def get_select_language_button(self, selector='a.header__lang-item'):
+    def get_language_button(self, selector='a.header__lang-item'):
         button = WebDriverWait(self.browser, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
         return button
 
-    def get_select_english_button(self, selector='//span[text()="English"]'):
+    def get_english_button(self, selector='//span[text()="English"]'):
         button = WebDriverWait(self.browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
@@ -22,20 +22,20 @@ class FootballMenuButtons:
     def __init__(self):
         self.browser = None
 
-    def get_select_football_category_button(self,
-                                            selector='//div[contains(@class, "vertical-panel__grow")]'
-                                                     '//div[contains(@class, "filter-component-row-container")][1]'
-                                                     '//span[contains(@class, "filter-component-expander__arrow--5izki")]'):
+    def get_football_category_button(self,
+                                     selector='//div[contains(@class, "vertical-panel__grow")]'
+                                              '//div[contains(@class, "filter-component-row-container")][1]'
+                                              '//span[contains(@class, "filter-component-expander__arrow--5izki")]'):
         button = WebDriverWait(self.browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
 
-    def get_select_show_all_button(self, selector='//span[text()="Show all"]'):
+    def get_show_all_button(self, selector='//span[text()="Show all"]'):
         button = WebDriverWait(self.browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
 
-    def get_select_all_leagues_buttons(
+    def get_all_leagues_buttons(
             self,
             selector='//div[contains(@class, "vertical-panel__grow")]'
                      '//div[contains(@class, "filter-component-row-container")][1]'
