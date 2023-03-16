c96e1a40a9429b66f2c356f7fa983fcf032ebe8f
“Baalt” <enjoylifebalt@gmail.com>
2023-03-17 00:00:55 +0200

    Added automation_schedule module with ScheduleManager class for managing and automating the football schedule. Includes classes for browser automation, match collection, and sports data management

---
+++ live/control_units/managers/automation_schedule.py
@@ -0,0 +1,29 @@
+from browser.browser import HeadlessChromeDriver
+from live.control_units.managers.tasks.main_operations import BrowserPreparer, FootballMenuHandler
+from live.control_units.managers.tasks.team_browser_list_collector import MatchCollector
+from live.control_units.scripers.schedule_data_collector import ScheduleScraper
+
+
+class ScheduleManager:
+    def __init__(self, smart_dict: dict):
+        self.driver = HeadlessChromeDriver(headless=False)
+        self.driver.maximize_window()
+        self.matches = {}
+        self.smart_dict = smart_dict
+
+    def run(self):
+        controller_1 = BrowserPreparer(driver=self.driver)
+        controller_1.open_page()
+        controller_1.switch_language()
+
+        controller_2 = FootballMenuHandler(driver=self.driver)
+        controller_2.open_main_football_menu()
+        controller_2.open_full_leagues_list()
+        controller_2.open_all_football_leagues()
+
+        controller_3 = ScheduleScraper(html=self.driver.get_page_html())
+
+        controller_4 = MatchCollector(matches=self.matches)
+        controller_4.collect_matches(schedule_dict=controller_3.extract_commands_to_dict(),
+                                     smart_dict=self.smart_dict)
+        return controller_4.get_matches()
