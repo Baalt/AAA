8788ec5fa25e6d4e8e501cdfb3dd50de8de745bd
“Baalt” <enjoylifebalt@gmail.com>
2023-03-21 02:06:04 +0200

    Added method to GameButtons class: is_match_button_clicked() to check if the match button is clicked.

--- browser/live_buttons/game_buttons.py
+++ browser/live_buttons/game_buttons.py
@@ -1,3 +1,4 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,6 +12,14 @@ class GameButtons:
         button = WebDriverWait(self.browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
+
+    def is_match_button_clicked(self, selector="//div[contains(@class, 'tab--4RNtVz _state_selected--408s1N') and contains(text(), 'Match')]"):
+        try:
+            button = self.browser.find_element(By.XPATH, selector)
+            if button:
+                return True
+        except NoSuchElementException:
+            return False
 
     def get_stats_button(self, selector="//div[contains(@class, 'tab--4RNtVz') and contains(text(), 'Team stats')]"):
         button = self.browser.find_element(By.XPATH, selector)
--- browser/live_buttons/menu_buttons.py
+++ browser/live_buttons/menu_buttons.py
@@ -26,12 +26,12 @@ class FootballMenuButtons:
                                      selector='//div[contains(@class, "vertical-panel__grow")]'
                                               '//div[contains(@class, "filter-component-row-container")][1]'
                                               '//span[contains(@class, "filter-component-expander__arrow--5izki")]'):
-        button = WebDriverWait(self.browser, 10).until(
+        button = WebDriverWait(self.browser, 20).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
 
     def get_show_all_button(self, selector='//span[text()="Show all"]'):
-        button = WebDriverWait(self.browser, 10).until(
+        button = WebDriverWait(self.browser, 20).until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         return button
 
@@ -40,6 +40,6 @@ class FootballMenuButtons:
             selector='//div[contains(@class, "vertical-panel__grow")]'
                      '//div[contains(@class, "filter-component-row-container")][1]'
                      '//span[contains(@class, "filter-component-expander--2CxuU")]'):
-        buttons = WebDriverWait(self.browser, 10).until(
+        buttons = WebDriverWait(self.browser, 20).until(
             EC.visibility_of_all_elements_located((By.XPATH, selector)))
         return buttons
