f065ea9eff6eca701924264e083c2ffb13119c6d
“Baalt” <enjoylifebalt@gmail.com>
2023-03-26 22:54:11 +0300

    small fix and improvements

--- browser/smart_buttons/smart_buttons.py
+++ browser/smart_buttons/smart_buttons.py
@@ -1,5 +1,8 @@
-from selenium.webdriver.common.by import By
+import re
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SmartStatButtons:
     def __init__(self):
@@ -14,19 +17,23 @@ class SmartStatButtons:
         # Return the sorted buttons list
         return filtered_buttons
 
-    def get_other_button(self, button_text):
-        selector = f"//div[@class='btn-group py-1 mr-2 stat-picker']//button[normalize-space()='{button_text}']"
+    def get_other_button(self, selector="//button[contains(@class, 'dropdown-toggle')]"):
         button = self.browser.find_element(By.XPATH, selector)
         return button
 
-    def get_drop_down_button(self, button_text):
-        selector = f"//a[@class='dropdown-item' and normalize-space()='{button_text}']"
-        button = self.browser.find_element(By.XPATH, selector)
+    def get_drop_down_button(self, button_text, timeout=10):
+        selector = f"//a[@class='dropdown-item' and contains(text(), '{button_text}')]"
+        wait = WebDriverWait(self.browser, timeout)
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, selector)))
         return button
 
-    def get_previous_season_button(self, idx):
-        button = self.browser.find_elements(By.XPATH, "//div[@id='teamsSeasons']//button")
-        return button[idx]
+    def get_previous_season_buttons(self):
+        buttons = self.browser.find_elements(By.XPATH, "//div[@id='teamsSeasons']//button")
+        matching_buttons = []
+        for button in buttons:
+            if re.search('\d{4}', button.text):
+                matching_buttons.append(button)
+        return matching_buttons
 
     def get_refresh_button(self):
         selector = "//button[contains(text(),'Обновить (Enter)')]"
