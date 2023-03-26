11bd85f4f37df742659e4989b3a531565263f7a7
“Baalt” <enjoylifebalt@gmail.com>
2023-03-26 22:47:04 +0300

    small fix and code improvement

--- line/control_units/managers/tasks/league_data_collector.py
+++ line/control_units/managers/tasks/league_data_collector.py
@@ -16,71 +16,40 @@ class LeagueDataCollector:
         self.driver = driver
         self.data = {}
 
-    def show_data(self):
+    def show_data(self) -> None:
         from pprint import pprint
         pprint(self.data)
 
-    def scrape_data(self):
-        self.data['current_season'] = {}
-        self.scraper = LeagueScraper(data=self.data['current_season'])
-        for button in self.driver.buttons.get_smart_stats_buttons():
-            button.click()
-            try:
-                self.driver.buttons.get_refresh_button().click()
-            except NoSuchElementException:
-                pass
-            self.wait_for_elements()
-            soup = BeautifulSoup(self.driver.get_page_html(), 'lxml')
-            self.scraper.from_soup(soup=soup, key=stats_dict[button.text.strip()])
-
-        self.driver.buttons.get_other_button().click()
-        self.driver.buttons.get_drop_down_button(button_text='Удары от ворот').click()
-        self.driver.buttons.get_refresh_button().click()
-        self.wait_for_elements()
-
-        button = self.driver.buttons.get_other_button()
-        soup = BeautifulSoup(self.driver.get_page_html(), 'lxml')
-        self.scraper.from_soup(soup=soup, key=stats_dict[button.text.strip()])
-
+    def scrape_data(self) -> None:
+        self.scrape_season('current_season')
         self.driver.driver.refresh()
-
         self.driver.buttons.get_previous_season_buttons()[-2].click()
         self.driver.buttons.get_previous_season_buttons()[-1].click()
-        self.driver.buttons.get_refresh_button().click()
+        self.scrape_season('previous_season')
 
-
-        self.data['previous_season'] = {}
-        self.scraper = LeagueScraper(data=self.data['previous_season'])
+    def scrape_season(self, season_key: str) -> None:
+        self.data[season_key] = {}
+        self.scraper = LeagueScraper(data=self.data[season_key])
         for button in self.driver.buttons.get_smart_stats_buttons():
             button.click()
-            try:
-                self.driver.buttons.get_refresh_button().click()
-            except NoSuchElementException:
-                pass
+            self.refresh_page()
             self.wait_for_elements()
             soup = BeautifulSoup(self.driver.get_page_html(), 'lxml')
             self.scraper.from_soup(soup=soup, key=stats_dict[button.text.strip()])
 
         self.driver.buttons.get_other_button().click()
         self.driver.buttons.get_drop_down_button(button_text='Удары от ворот').click()
-        self.driver.buttons.get_refresh_button().click()
+        self.refresh_page()
         self.wait_for_elements()
-
         button = self.driver.buttons.get_other_button()
         soup = BeautifulSoup(self.driver.get_page_html(), 'lxml')
         self.scraper.from_soup(soup=soup, key=stats_dict[button.text.strip()])
 
+    def refresh_page(self) -> None:
+        try:
+            self.driver.buttons.get_refresh_button().click()
+        except NoSuchElementException:
+            pass
 
-    def wait_for_elements(self):
-        WebDriverWait(self.driver.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*')))
-
-
-if __name__ == '__main__':
-    driver = SmartChromeDriver()
-    driver.maximize_window()
-    driver.open_page('https://smart-tables.ru')
-    driver.login(username='enjoylifebalt@gmail.com', password='astraSTb00rato')
-
-    collector = LeagueDataCollector(driver=driver)
-    collector.scrape_data()
-    collector.show_data()
+    def wait_for_elements(self) -> None:
+        WebDriverWait(self.driver.driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//*')))
